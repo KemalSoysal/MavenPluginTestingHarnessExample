e65317dee5880dc1f662b50fa2f3c220fd8a49e3
Kemal Soysal <Kemal.Soysal@ls-it-solutions.de>
2021-08-27 11:13:49 +0200

    add gitignore and the produced project

---
+++ my-maven-plugin/src/test/java/org/apache/maven/plugin/my/MyMojoTest.java
@@ -0,0 +1,54 @@
+package org.apache.maven.plugin.my;
+
+import java.io.File;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+public class MyMojoTest
+    extends AbstractMojoTestCase
+{
+    /** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        // required
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        // required
+        super.tearDown();
+    }
+
+    /**
+     * @throws Exception if any
+     */
+    public void testSomething()
+        throws Exception
+    {
+        File pom = getTestFile( "src/test/resources/unit/project-to-test/pom.xml" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        MyMojo myMojo = (MyMojo) lookupMojo( "touch", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+    }
+    
+    /**
+     * @throws Exception if any
+     */
+    public void testSomethingSimple()
+        throws Exception
+    {
+      MyMojo myMojo = new MyMojo();
+      Object outputDirectory = getVariableValueFromObject(myMojo, "outputDirectory");
+      assertNull(String.format("outputDirectory should have not been initialized",outputDirectory), outputDirectory );
+      setVariableValueToObject(myMojo, "outputDirectory", new File("target"));
+      outputDirectory = getVariableValueFromObject(myMojo, "outputDirectory");
+      assertNotNull( outputDirectory );
+      assertEquals(String.format("outputDirectory should have been set to %s",outputDirectory), getVariableValueFromObject(myMojo, "outputDirectory"), outputDirectory);
+    }
+}
